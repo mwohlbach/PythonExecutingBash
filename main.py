7530de0a9b560d0c6c377f1aaf3dbb62774c1c24
Matthew Wohlbach <mwohlbach@Matthews-MacBook-Pro.local>
2018-03-29 13:46:22 -0400

    signal with error catching on timeout

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import subprocess
+import signal
 
-output = subprocess.check_output(['bash','-c', 'git add .'])
+def handler(signum,frame):
+    print('Forever is ovah!!!')
+    raise Exception("end of time")
 
-print(str(output))
+def loop_forever():
+    import time
+    while 1:
+        print ("sec")
+        time.sleep(1)
+
+
+signal.signal(signal.SIGALRM, handler)
+
+signal.alarm(10)
+
+try:
+    loop_forever()
+except Exception as exc:
+    print (exc)
+
+# output = subprocess.check_output(['bash','-c', 'git commit -m "hi mom"'])
+
+
+
+# print(str(output))
